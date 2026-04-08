5c9416e8d10a7e483d0d93ead32190291a81bd41
Ben Jones <fetiddius@gmail.com>
2026-04-08 20:42:35 +0100

    try on rpi

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = std::net::TcpListener::bind("192.168.1.66:53").unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut s) => {
+                let mut buf = String::new();
+                s.read_to_string(&mut buf).unwrap();
+                println!("{buf}");
+            }
+            Err(e) => println!("{e}"),
+        }
+    }
 }
